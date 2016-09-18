7e6b124b708f2f5214e70a93cad26705f6c55440
Thomas <thomas.horga@gmail.com>
2016-09-17 21:03:30 -0400

    Fixed everything broken
    No sleep idk anymore

---
+++ app/src/main/java/com/ThomasHorgaGmailCom/JustMaBitCnx/MainActivity.java
@@ -0,0 +1,137 @@
+package com.ThomasHorgaGmailCom.JustMaBitCnx;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.ThomasHorgaGmailCom.JustMaBitCnx.estimote.BeaconID;
+import com.ThomasHorgaGmailCom.JustMaBitCnx.estimote.EstimoteCloudBeaconDetails;
+import com.ThomasHorgaGmailCom.JustMaBitCnx.estimote.EstimoteCloudBeaconDetailsFactory;
+import com.ThomasHorgaGmailCom.JustMaBitCnx.estimote.ProximityContentManager;
+import com.estimote.sdk.SystemRequirementsChecker;
+import com.estimote.sdk.cloud.model.Color;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private static final Map<Color, Integer> BACKGROUND_COLORS = new HashMap<>();
+
+    static {
+        BACKGROUND_COLORS.put(Color.ICY_MARSHMALLOW, android.graphics.Color.rgb(109, 170, 199));
+        BACKGROUND_COLORS.put(Color.BLUEBERRY_PIE, android.graphics.Color.rgb(98, 84, 158));
+        BACKGROUND_COLORS.put(Color.MINT_COCKTAIL, android.graphics.Color.rgb(155, 186, 160));
+    }
+
+    private static final int BACKGROUND_COLOR_NEUTRAL = android.graphics.Color.rgb(160, 169, 172);
+
+    private ProximityContentManager proximityContentManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+
+        proximityContentManager = new ProximityContentManager(this,
+                Arrays.asList(
+                        new BeaconID("B9407F30-F5F8-466E-AFF9-25556B57FE6D", 41991, 17469)),
+                new EstimoteCloudBeaconDetailsFactory());
+        proximityContentManager.setListener(new ProximityContentManager.Listener() {
+            @Override
+            public void onContentChanged(Object content) {
+                String text;
+                Integer backgroundColor;
+                if (content != null) {
+                    EstimoteCloudBeaconDetails beaconDetails = (EstimoteCloudBeaconDetails) content;
+
+                    text = "You're in " + beaconDetails.getBeaconName() + "'s range!";
+                    backgroundColor = BACKGROUND_COLOR_NEUTRAL; // green
+                    // Assuming you are using xml layout
+                    System.out.print('d');
+                    Button button = (Button) findViewById(R.id.buttonTip);
+
+                    Log.d("myTag","This is  a log ");
+                    button.setOnClickListener(new View.OnClickListener() { //com.example.tom_h.coinbase_implementation://oauth.callback$code=code
+                        public void onClick(View arg0) { //urn:ietf:wg:oauth:2.0:oob
+                            Intent viewIntent =
+                                    new Intent("android.intent.action.VIEW",
+                                            Uri.parse("https://www.coinbase.com/oauth/authorize?response_type=code&client_id=856fb5f74b2f139c88d49f44ace25eb67bf26ec0089ad5e2ca6d5b9f96864d22&redirect_uri=https://x-com.example.tom_h.coinbase_implementation://oauth.callback$code=code&scope=send&meta[send_limit_amount]=1&meta[send_limit_currency]=USD&meta[send_limit_period]=day"));
+                            startActivity(viewIntent);
+
+                            //System.out.println(authzCode);
+                        }
+                    });
+
+                    text = "John is performing near you!";
+                    backgroundColor = 0xff90ff99; // green
+
+
+
+                } else {
+                    text = "No performers in range.";
+                    backgroundColor = 0xff444444; //some color
+                }
+                ((TextView) findViewById(R.id.textView)).setText(text);
+                findViewById(R.id.relativeLayout).setBackgroundColor(
+                        backgroundColor != null ? backgroundColor : BACKGROUND_COLOR_NEUTRAL);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (!SystemRequirementsChecker.checkWithDefaultDialogs(this)) {
+            Log.e(TAG, "Can't scan for beacons, some pre-conditions were not met");
+            Log.e(TAG, "Read more about what's required at: http://estimote.github.io/Android-SDK/JavaDocs/com/estimote/sdk/SystemRequirementsChecker.html");
+            Log.e(TAG, "If this is fixable, you should see a popup on the app's screen right now, asking to enable what's necessary");
+        } else {
+            Log.d(TAG, "Starting ProximityContentManager content updates");
+            proximityContentManager.startContentUpdates();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "Stopping ProximityContentManager content updates");
+        proximityContentManager.stopContentUpdates();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        proximityContentManager.destroy();
+    }
+    public void imageClick(View view) {
+         /*
+         code to load a profile or something, ratings etc.
+         */
+
+    }
+
+    public void tipButton(View view) {
+        /* code to implement tipping
+         if (balance - 1 >= 0){
+        }
+        else {
+            // you poor
+        }*/
+    }
+}
---
+++ app/src/main/java/com/ThomasHorgaGmailCom/JustMaBitCnx/estimote/MyAppCallback.java
@@ -0,0 +1,202 @@
+package com.ThomasHorgaGmailCom.JustMaBitCnx.estimote;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.coinbase.api.Coinbase;
+import com.coinbase.api.CoinbaseBuilder;
+import com.coinbase.api.entity.Transaction;
+import com.coinbase.api.exception.CoinbaseException;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import org.joda.money.Money;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+/**
+ * Created by Tom-H on 9/17/2016.
+ */
+public class MyAppCallback extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+
+
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "MyAppCallback Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.tom_h.coinbase_implementation/com.example.tom_h.coinbase_implementation/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    // HTTP POST request
+    private String sendPost(String token) throws Exception {
+
+        final String USER_AGENT = "Mozilla/5.0";
+
+        String url = "https://api.coinbase.com/oauth/token";
+        URL obj = new URL(url);
+        HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+
+        //add reuqest header
+        con.setRequestMethod("POST");
+        //con.setRequestProperty("User-Agent", USER_AGENT);
+        //con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+        //String urlParameters = "client_id=856fb5f74b2f139c88d49f44ace25eb67bf26ec0089ad5e2ca6d5b9f96864d22&redirect_uri=urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob%0D&response_type=code&scope=wallet%3Auser%3Aread";
+        //
+        //
+        String urlParameters = "grant_type=authorization_code&code="+token+"&client_id=856fb5f74b2f139c88d49f44ace25eb67bf26ec0089ad5e2ca6d5b9f96864d22&client_secret=ec2eb5737e2309fb08d2e498920c928b51be9b561696ed0992b7d3781d2ef861&redirect_uri=https://x-com.example.tom_h.coinbase_implementation://oauth.callback$code=code";
+        //https://www.coinbase.com/oauth/authorize?client_id=856fb5f74b2f139c88d49f44ace25eb67bf26ec0089ad5e2ca6d5b9f96864d22&redirect_uri=urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob%0D&response_type=code&scope=wallet%3Auser%3Aread
+        // Send post request
+        con.setDoOutput(true);
+        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+        wr.writeBytes(urlParameters);
+        wr.flush();
+        wr.close();
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'POST' request to URL : " + url);
+        System.out.println("Post parameters : " + urlParameters);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            Log.d("myTag",inputLine);
+            response.append(inputLine);
+
+        }
+        in.close();
+        Log.d("myTag","sendPost Finished");
+        //print result
+        System.out.println(response.toString());
+        return response.toString();
+
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+
+        StrictMode.setThreadPolicy(policy);
+
+        String token = getIntent().getData().getQueryParameter("code");
+
+        String access_token = "";
+
+        try {
+            String authInfo = sendPost(token);
+            access_token = authInfo.substring((authInfo.indexOf("\"access_token\":")+16),authInfo.indexOf(",\"token_type\"")-1);
+            Log.d("myTag",access_token);
+        } catch (Exception e) {
+            Log.d("myTag","sendPost failed");
+            e.printStackTrace();
+            return;
+        }
+
+
+
+        Log.d("myTag",access_token + "asd");
+        Coinbase cb = new CoinbaseBuilder()
+                .withAccessToken(access_token)
+                .build();
+
+
+        /*Coinbase cb = new CoinbaseBuilder()
+                .withApiKey(System.getenv("G2KbsMi40ZLAxn9s"), System.getenv("JY59cIL4t2Ppil7QFWQ5dBHSpyFggOyJ"))
+                .build();*/
+
+        Log.d("myTag",access_token);
+
+        /*
+        try {
+            Log.d("myTag", cb.getUser().getEmail());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (CoinbaseException e) {
+            e.printStackTrace();
+        }
+        */
+
+        Transaction t = new Transaction();
+        t.setTo("jacobsolis123@gmail.com");
+        t.setAmount(Money.parse("BTC 0.00001"));
+        //Transaction r = cb.sendMoney(t);
+        try {
+            Transaction r = cb.sendMoney(t);
+            Log.d("myTag", "Money sent");
+        } catch (CoinbaseException e) {
+            Log.d("myTag", "Money Failed");
+            e.printStackTrace();
+        } catch (IOException e) {
+            Log.d("myTag", "Money Failed");
+            e.printStackTrace();
+        }
+
+        //String test = getIntent().getData().getQueryParameter("code");
+        //Log.d("myTag",test + "asd");
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "MyAppCallback Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.tom_h.coinbase_implementation/com.example.tom_h.coinbase_implementation/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+}
+
+
